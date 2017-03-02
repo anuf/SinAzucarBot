8ebc0b239fbb7564fc44fe968633e1f7e5621c66
fito <fitob612@gmail.com>
2017-03-02 20:00:04 +0100

    Add i18n with gettext. Provide Spanish and Galician (temporal) translations.

--- sin_azucar_bot.py
+++ sin_azucar_bot.py
@@ -8,14 +8,24 @@ from bs4 import BeautifulSoup
 import random
 import time
 
+import gettext
+
+# Set up message catalog access
+
+t = gettext.translation(
+    'sin_azucar_bot', 'locale',
+    fallback=True,
+)
+_ = t.gettext
+
 bot = tb.TeleBot(TOKEN)
 
 commands = {  # command description used in the "help" command
-              'start': 'Get used to the bot',
-              'help': 'Gives you information about the available commands',
-              'product [name]': 'Retrieves a given product or a random one',
-              'list [letter]': 'Show all available products starting with a given letter',
-              'hist': 'List last 5 products shown'
+              'start': _('Get used to the bot'),
+              'help': _('Gives you information about the available commands'),
+              'product '+_('[name]'): _('Retrieves a given product or a random one'),
+              'list '+_('[letter]'): _('Show all available products starting with a given letter'),
+              'hist': _('List last 5 products shown')
 }
 
 MAIN_URL = "http://www.sinazucar.org"   
@@ -27,7 +37,7 @@ hist = []
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     '''This handlert shows a welcome message.'''
-    welcome_message = "Howdy!"
+    welcome_message = _("Howdy!")
     bot.reply_to(message, welcome_message)
     start_time = time.time()
     load_data()
@@ -41,7 +51,7 @@ def command_help(message):
     Display the commands and what are they intended for.
     '''
     chat_id = message.chat.id
-    help_text = "The following commands are available: \n"
+    help_text = _("The following commands are available: \n")
     for key in commands:  # generate help text out of the commands dictionary defined at the top
         help_text += "/" + key + ": "
         help_text += commands[key] + "\n"
@@ -88,7 +98,7 @@ def get_list(message):
             if k[0].lower() == arguments:
                 short_list.append(k)
         if len(short_list) == 0:
-            bot.send_message(chat_id,"No products found starting with {0}".format(arguments))
+            bot.send_message(chat_id,_("No products found starting with {0}").format(arguments))
         else:
             bot.send_message(chat_id,'\n'.join(sorted(short_list)))
     else: # no arguments
@@ -103,7 +113,7 @@ def show_hist(message):
     if len(hist) > 0:
         bot.send_message(chat_id, '\n'.join(hist))
     else:
-        bot.send_message(chat_id,"Hist is empty")
+        bot.send_message(chat_id,_("Hist is empty"))
 
 @bot.message_handler(commands=['random'])
 def get_random_product(message):
@@ -132,7 +142,7 @@ def get_random_product(message):
                 bot.send_photo(chat_id, image)
                 update_hist(entries[random_product_index]['title'])
     else:
-        bot.send_message(chat_id, "The site seems to be unavailable at the moment. Please, try again later.")
+        bot.send_message(chat_id, _("The site seems to be unavailable at the moment. Please, try again later."))
 
 def update_hist(prod_name):
     global hist
@@ -164,8 +174,7 @@ def load_data():
         print("END load_data()")
         return npages
     else:
-        return "The site seems to be unavailable at the moment. \
-        Please, try again later."
+        return _("The site seems to be unavailable at the moment. Please, try again later.")
 
 def get_num_pages(html):
     '''
@@ -207,9 +216,9 @@ def findMe(param):
                             image = subsoup.find('img',{'class':'avia_image '})['src']
                             return info, image, entry['title']
     else:
-        return "The site seems to be unavailable at the moment. Please, try again later.", None, None
+        return _("The site seems to be unavailable at the moment. Please, try again later."), None, None
 
-    return "Item not found.", None, None
+    return _("Item not found."), None, None
 
 
 def build_items_dictionary(num_pages):
